a7aec58c85fe6bb1ceb794ab7e05e10d3ec014a1
sisindri-admin <sisindrimacha9988@gmail.com>
2026-08-19 14:02:50 +0530

    update maindart

--- lib/main.dart
+++ lib/main.dart
@@ -32,13 +32,8 @@ class Flash2MartApp extends StatelessWidget {
       
       // పేజీ నావిగేషన్ Routes
       routes: {
-<<<<<<< HEAD
         '/auth': (context) => const MerchantAuthScreen(),
         '/dashboard': (context) => const MerchantDashboard(),
-=======
-        '/auth': (context) => MerchantAuthScreen(),
-        '/dashboard': (context) => MerchantDashboardScreen(),
->>>>>>> 441a9e7 (updated build fix)
       },
     );
   }
