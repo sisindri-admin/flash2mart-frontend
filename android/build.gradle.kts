f1f96f4fb551a9ab6780b2e99d64d2228362ac2c
sisindri-admin <sisindrimacha9988@gmail.com>
2026-08-24 17:27:51 +0530

    Fix gradle plugin classpath conflict

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,13 +5,6 @@ allprojects {
     }
 }
 
-plugins {
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("dev.flutter.flutter-gradle-plugin") apply false
-}
-
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
